f1d372b0391c063212fa12f1f5938ee4bbec49e4
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-01-26 14:16:57 +0800

    score and input

--- application.js
+++ application.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
       timeNode.text(parseInt(timeNode.text()) - 1);
       if (parseInt(timeNode.text()) <=0) {
         clearInterval(gameTimerIntervalId);
+        endGame();
       }
     };
     var gameTimerIntervalId = setInterval(countdown,1000);
@@ -19,5 +20,15 @@ $(document).ready(function() {
 // falling letters = generate random alphabets, make it fall from the game div, falls faster as time passes
 
 // life reduction = life div removed when player presses wrong key or letter falls to ground
+
+//end game = end game when time is over, show separate div
+  var endGame = function() {
+    var newElem = '<div class="announceScore">Player score: ' +
+                    '<div class="input">' +
+                      '<input class="playerName" value="Please enter name"' +
+                    '</div>' +
+                  '</div>';
+    $('.gamePage').append(newElem);
+  };
 
 });
