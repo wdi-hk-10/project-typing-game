6066bc6f0c65af1143dc92ac99b5d484e94ffef0
Jinny Lee <Jinnylee@Jinnys-MacBook-Air.local>
2016-01-26 18:14:05 +0800

    reduce all lives and end game

--- application.js
+++ application.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
   var gamePage = $('.gamePage');
+  var life = 4;
 
 // start game = press start button, timer starts
   var startGameCountdown = function() {
     var startButton = $('.start').hide();
-    fallingStart();
+    var startInterval = setInterval(fallingStart, 1500);
 
     var countdown = function() {
       var timeNode = $('#seconds');
       timeNode.text(parseInt(timeNode.text()) - 1);
-      if (parseInt(timeNode.text()) <=0) {
+      if (life==0){
         clearInterval(gameTimerIntervalId);
+        clearInterval(startInterval);
+        $('.gamePage').hide();
+        $('.gameOver').show();
+      } else if (parseInt(timeNode.text())<=0) {
+        clearInterval(gameTimerIntervalId);
+        clearInterval(startInterval);
         endGame();
       }
     };
@@ -21,29 +28,38 @@ $(document).ready(function() {
 
 // falling letters = generate random alphabets, make it fall from the game div, falls faster as time passes
 
-  var num = Math.floor(Math.random()*(26))+65;
-  var convertToLetter = String.fromCharCode(num);
-  var droppingPosition = Math.floor(Math.random()*$('.gamePage').width());
   var fallingStart = function() {
-      var $fallingLetter = $('<div class="fallingLetters">'+convertToLetter+'</div>').appendTo(gamePage);
-      $fallingLetter.css({
-        'left': droppingPosition +'px',
-        'fontSize':'35px',
-        'border':'solid 2px',
-        'padding': '0px 10px 0px 10px'
-      });
-      $fallingLetter.animate({
-          top: '545px'
-        },{
-          duration: 3000,
-          complete: function() {
-            $(this).remove();
-          }
+    var num = Math.floor(Math.random()*(26))+65;
+    var convertToLetter = String.fromCharCode(num);
+    var droppingPosition = Math.floor(Math.random()*$('.gamePage').width());
+    var $fallingLetter = $('<div class="fallingLetters">'+convertToLetter+'</div>').appendTo(gamePage);
+    $fallingLetter.css({
+      'left': droppingPosition +'px',
+      'fontSize':'35px',
+      'border':'solid 2px',
+      'padding': '0px 10px 0px 10px'
+    });
+    $fallingLetter.animate({
+        top: '545px'
+      },{
+        duration: 3000,
+        complete: function () {
+          $(this).remove();
+          reduceLife();
         }
-      );
+      }
+    );
   };
 
 // life reduction = life div removed when player presses wrong key or letter falls to ground
+  var reduceLife = function(){
+    if (life > 0){
+      $('.life:nth-child(' + life + ')').hide();
+      life--;
+    };
+  };
+
+
 
 //end game = end game when time is over, show separate div
   var endGame = function() {
